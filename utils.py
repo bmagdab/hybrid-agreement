0a059ec81b4a18c7d78bce2cf5c707c6645f887c
putor <mj.borysiak@student.uw.edu.pl>
2025-06-03 19:06:44 +0200

    added a function for comparing morphological descriptions

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ path = 'NKJP\\'
 # filelist = [f'NKJP_300M_{x:03d}' for x in range(starting_number, 191)]
 filelist = ['NKJP_1M']
 
+
 def read_conllu(file):
     # reading the conllu file
     with open(path + file + '.conllu', mode='r', encoding='utf-8') as nkjp:
@@ -27,13 +28,13 @@ def read_conllu(file):
             # getting the ID of the sentence in the file
             sent_id = re.findall(r'# id = (\d+)', line)
             if sent_id:
-                sent_dict['id'] = sent_id[0]
+                sent_dict['id'] = int(sent_id[0])
 
             # saving each word as a dictionary of all the information I need
             if re.match(r'\d', line):
                 word = line.split('\t')
                 sent_dict['words'].append({'id': int(word[0]),
-                                           'form': word[1],
+                                           'form': word[1] if 'depr' in word[4] else word[1].lower(),
                                            'POS': word[3],
                                            'morph_descr': word[4],
                                            'feats': word[5],
@@ -41,6 +42,24 @@ def read_conllu(file):
                                            'label': word[7]})
         corpus.append(sent_dict)
     return corpus
+
+
+def compare_morph_descr(found_word, reference_table):
+    matching_entries = reference_table[reference_table.form == found_word['form'].lower()].morph_descr.values
+    same = False
+    for entry in matching_entries:
+        ref_descr_str = entry
+        found_descr = [feature.split('.') for feature in found_word['morph_descr'].split(':')]
+        ref_descr = [feature.split('.') for feature in ref_descr_str.split(':')]
+        same = True
+        for i, feature in enumerate(found_descr):
+            for value in feature:
+                if value not in ref_descr[i]:
+                    same = False
+                    break
+            if not same:
+                break
+    return same
 
 
 def write_json():
