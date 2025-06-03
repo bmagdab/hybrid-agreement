9ae64db6bb9afd90c92e1a981dcbc869a0c7565e
putor <mj.borysiak@student.uw.edu.pl>
2025-06-03 20:54:47 +0200

    reorganised the script

--- main.py
+++ main.py
@@ -2,25 +2,27 @@ from tqdm import tqdm
 import pandas as pd
 from utils import *
 from get_info import get_info
+from datetime import datetime
 
 
 def main():
     noun_forms = pd.read_csv('noun_forms.csv')
     form_list = noun_forms.form.values
 
-    for file in tqdm(filelist):
-        file = read_conllu(file)
+    for file in filelist:
+        print(f'{datetime.now()} -- processing {file}.conllu')
+        conllu = read_conllu(file)
         found_words = []
 
-        for sentence in file:
+        for sentence in tqdm(conllu):
             for word in sentence['words']:
                 if ((word['form'] in form_list and
                      compare_morph_descr(word, noun_forms)) or
                         'depr' in word['morph_descr']):
                     found = get_info(word, sentence, noun_forms)
                     found_words.append(found)
-
-        # write_json(found_words)
+        print(f'{file}.conllu done, writing {file}.json')
+        write_json(found_words, file)
 
 
 main()
