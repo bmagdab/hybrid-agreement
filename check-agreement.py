a024c263605b2973fcce1d000f5c44df9743f0d3
putor <mj.borysiak@student.uw.edu.pl>
2025-06-09 11:53:23 +0200

    added comments

--- check-agreement.py
+++ check-agreement.py
@@ -6,8 +6,10 @@ import os
 
 starting_number = 0
 json_path = 'occurrences\\'
-# files = [f'NKJP_300M_{x:03d}' for x in range(starting_number, 191)]
-files = ['NKJP_1M']
+files = [f'NKJP_300M_{x:03d}' for x in range(starting_number, 191)]
+# files = ['NKJP_1M']
+
+# a dictionary of social genders associated with each lexeme in the list of socially gendered nouns
 gendered_dict = {
     'babochłop': 'f',
     'babol': 'f',
@@ -29,26 +31,47 @@ gendered_dict = {
     'pasztet': 'f',
     'wamp': 'f'
 }
+
 # list of symbols marking the verb flexemes that agree with the subject in gender
 verb_like = ['praet', 'pact', 'ppas']
 
 
 def file_into_dict(file_name):
+    """
+    reads the file into a list of dictionaries, where each dictionary corresponds to an occurrence of a word found in
+    the corpus with its head and dependencies
+    :param file_name: name of the file to be read, without the .json extension
+    :return: list of words as dictionaries
+    """
     with open(json_path + file_name + '.json', mode='r', encoding='utf-8') as source:
         occurrence_list = json.load(source)
     return occurrence_list
 
 
 def get_source_info(occurrence):
+    """
+    for a given occurrence of the word finds the information about the word (and not any of its dependencies!), which is
+    the source of agreement, and information about where the word was found
+    :param occurrence: a dictionary of information about the found word
+    :return: a dictionary of information about the word, but restructured :)
+    """
     if occurrence['category'] == 'profession':
+        # if the noun refers to a profession, it's commonly used for men so the alternative gender is feminine
         other_gender = 'f'
     elif occurrence['category'] == 'depreciative':
+        # if the noun is in the depreciative form, it's marked as not m1 because of its shape,
+        # but "semantically" it's m1
         other_gender = 'm1'
     else:
+        # for the socially gendered nouns it depends on the specific lexeme, so the information is looked up in the
+        # dedicated dictionary
         other_gender = gendered_dict[occurrence['lexeme']]
 
-    if (re.search(r':n\b', entry['morph_descr']) and
-            re.search(r':pl\b', entry['morph_descr'])) and other_gender == 'f':
+    # if the noun is in plural and the hybrid agreement is between feminine and neuter, the occurrence is excluded
+    # because the agreeing word has the same form for plural feminine and plural neuter
+    if (re.search(r':pl\b', entry['morph_descr']) and
+            re.search(r':n\b', entry['morph_descr']) and
+            other_gender == 'f'):
         return False
 
     info = {
@@ -66,6 +89,11 @@ def get_source_info(occurrence):
 
 
 def get_target_info(target, info):
+    """
+    finds information about the given target of agreement
+    :param target: dictionary of information about the target of agreement
+    :param info: dictionary of information about the source of agreement
+    """
     if isinstance(target, list):
         gender = re.search(r':[mnf]\d?\b', target[1]).group()[1:]
         info['target_form'] = target[0]
@@ -78,6 +106,8 @@ def get_target_info(target, info):
         info['target_gender'] = re.search(r':[mnf]\d?\b', target['morph_descr']).group()[1:]
         info['type_of_agreement'] = 'relpron' if len(target) == 2 else 'attributive'
 
+    # all the information found for an occurrence of agreement is added to the dictionary that will be turned into
+    # a DataFrame
     for key in info.keys():
         out_dict[key].append(info[key])
 
@@ -93,8 +123,9 @@ def write_csv(file_name, out_dict):
     df.to_csv(out_path, index=False)
 
 
+# iterated through the .json files and saves the occurrences of agreement
 for file in files:
-    occurrences = file_into_dict(file)
+    occurrences = file_into_dict(file)  # list of words found in the corpus with the words that agree gender with them
     out_dict = {'file_id': [],
                 'sentence_id': [],
                 'sentence': [],
@@ -109,18 +140,25 @@ for file in files:
                 'target_gender': [],
                 'type_of_agreement': []         # attributive, predicative or relative pronoun
                 }
+
     for entry in occurrences:
         if not get_source_info(entry):
             continue
         source_info = get_source_info(entry)
-        if (entry['head'] != ['', '', ''] and
-                re.search(r'praet|pact|ppas', entry['head'][1]) and
-                re.search(r':nom', entry['morph_descr']) and
-                entry['head'][2] == 'nsubj'):
+
+        # there is predicative agreement only if...
+        if (entry['head'] != ['', '', ''] and                               # the word has a dependency head
+                re.search(r'praet|pact|ppas', entry['head'][1]) and  # the head is a verb inflected for gender
+                re.search(r':nom', entry['morph_descr']) and         # the word is in nominative case
+                entry['head'][2] == 'nsubj'):                               # the word is the subject
             get_target_info(entry['head'], source_info)
+
+        # iterates through the dependents, but saves them only if they are adjectives (attributive agreement)
         for dep in entry['dependents']:
             if 'adj:' in dep['morph_descr']:
                 get_target_info(dep, source_info)
+
+        # iterates through the relative pronouns that refer to the word, saves all of them
         for relpron in entry['rel_prons']:
             get_target_info(relpron, source_info)
 
--- get_info.py
+++ get_info.py
@@ -4,21 +4,28 @@ morf = morfeusz2.Morfeusz()
 
 
 def get_info(word, sentence, nouns_table):
+    """
+    for a given word in a sentence finds the information later required to check gender agreement with other words
+    :param word: a dictionary with information corresponding to the word of interest
+    :param sentence: a dictionary with information about the sentence, where the word was found
+    :param nouns_table: a pandas DataFrame with all forms of words that are searched in the corpus
+    :return: a dictionary with the required information about the word
+    """
     # finds the lexical form of the noun
     if word['form'] in nouns_table.form.values:
-        lexeme = nouns_table[nouns_table.form == word['form']].lexeme.values[0]
         # in the table with listed nouns
-    else:   # if it's not in the table it should be a depreciative form...
+        lexeme = nouns_table[nouns_table.form == word['form']].lexeme.values[0]
+    else:   # if it's not in the table it should be a depreciative form
         lexeme = ''
         for analysis in morf.analyse(word['form']):
             if re.search(r'depr|subst', analysis[2][2]) and analysis[2][3] != ['nazwisko']:
-                # Morfeusz gives a number of analyses, some of them might be not in depreciative, but it doesn't matter
-                # because here I'm looking for the lexeme; also I won't look at surnames
+                # Morfeusz gives a number of analyses, we are looking for the analyses of nouns (not e.g. a verb with
+                # the same form) and we want to exclude surnames
                 lexeme = re.match(r'\w+', analysis[2][1]).group()
         if not lexeme:
-            return False
+            return False    # if nothing is found, the function was run by mistake and we stop it
 
-    # finds the category from the table of listed nouns or from the morphological description
+    # finds the category of the noun from the table of listed nouns or from the morphological description
     if word['form'] in nouns_table.form.values:
         category = nouns_table[nouns_table.form == word['form']].category.values[0]
     elif 'depr' in word['morph_descr']:
@@ -29,7 +36,7 @@ def get_info(word, sentence, nouns_table):
 
     # iterates through the words in the sentence to find:
     # the dependency head of the word,
-    # its dependencies and the relative pronouns,
+    # the words dependencies,
     # its relative pronouns
     head = []
     dependents = []
@@ -39,7 +46,7 @@ def get_info(word, sentence, nouns_table):
         if other_word['id'] == word['head']:
             head = [other_word['form'], other_word['morph_descr'], word['label']]
 
-        # looks for the :relcl relation subtype
+        # for the relative pronouns looks for the :relcl relation subtype
         elif other_word['head'] == word['id'] and ':relcl' in other_word['label']:
             # the relative pronoun is attached to the main predicate of the relative clause, so we iterate again
             for another_word in sentence['words']:
