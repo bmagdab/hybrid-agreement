d7ed43eff82a1f050cc9ee3beaad48a18cbb7ac9
putor <mj.borysiak@student.uw.edu.pl>
2025-06-20 17:48:32 +0200

    developing stats

--- statistics/read-tables.R
+++ statistics/read-tables.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(ggplot2)
+library(stringr)
 
+# reading data
 path = "C:/Users/magda/moje/hybrid-agreement-corpus/agreement-occurrences"
 numbers <- sprintf("%03d", 0:190)
 filenames <- paste0("NKJP_300M_", numbers, ".csv")
@@ -8,6 +10,7 @@ filepaths <- file.path(path, filenames)
 
 data <- lapply(filepaths, read.csv) %>% bind_rows()
 
+# summaries
 data %>% 
   group_by(source_cat, type_of_agreement) %>% 
   summarise(prop = sum(source_other_gender == target_gender)/n())
@@ -16,6 +19,19 @@ data %>%
   group_by(type_of_agreement) %>% 
   summarise(dist = mean(abs(distance)))
 
+# filtering profession nouns
+data %>% 
+  filter(source_cat == 'profession') %>% 
+  filter(target_gender == 'f') -> prof
+
+prof %>% 
+  mutate(source_case = str_extract(source_morph_descr, 'nom|gen|acc|dat|loc|inst|voc'),
+         target_case = str_extract(target_morph_descr, 'nom|gen|acc|dat|loc|inst|voc')) -> prof
+
+prof %>% 
+  filter((source_case != target_case) & !(is.na(target_case))) -> what
+
+# filtering gendered nouns
 data %>% filter(source_cat == 'gendered') -> gend
 gend %>% 
   group_by(type_of_agreement) %>% 
@@ -25,6 +41,7 @@ gend %>%
     prop = condition/total
     )
 
+# filtering depreciative forms
 data %>% filter(source_cat == 'depreciative') -> depr
 depr %>% 
   group_by(type_of_agreement) %>% 
